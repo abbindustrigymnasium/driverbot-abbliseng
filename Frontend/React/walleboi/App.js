c5973e2c384d826abf3a9cf668e4e51bc4ebe89d
Lisa Engström <Lisa.Engstrom@abbindustrigymnasium.se>
2020-03-31 19:07:37 +0200

    app, uppstart och mqtt

--- Frontend/React/walleboi/App.js
+++ Frontend/React/walleboi/App.js
@@ -1,12 +1,75 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { isValidElement, Component } from 'react';
+import { StyleSheet,
+   Text,
+   View, 
+   TouchableOpacity, 
+   AsyncStorage 
+  } from 'react-native';
+import init from './node_modules/react_native_mqtt'
+init({
+  size: 10000,
+  storageBackend: AsyncStorage,
+  defaultExpires: 1000 * 3600 * 24,
+  enableCache: true,
+  reconnect: true,
+  sync : {
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  }
+})
+function onConnect() {
+  console.log("ye")
+  client.subscribe("abbexpectmore@gmail.com/light")
+}
+function onConnectionLost(responseObject){
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+function onMessageArrived(message) {
+  console.log("onMessageArrived:"+message.payloadString);
+}
+const client = new Paho.MQTT.Client("maqiatto.com", 8883, 'clientID');
+// Set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+client.connect({
+  userName: "abbexpectmore@gmail.com",
+  password: "hej",
+  onSuccess: onConnect,
+})
+
+
+export default class reactApp extends Component{
+  constructor(){
+    super()
+    this.state = {
+      myText: 'My Original Text'
+    }
+  }
+  updateText = () => {
+    this.setState({myText: 'My changed text!'})
+  }
+  sendMessage = () => {
+    var mess = `(0:0:90)`
+    var message = new Paho.MQTT.Message(mess);
+    message.destinationName = "abbexpectmore@gmail.com/ctrl";
+    client.send(message);
+  }
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text onPress = {this.updateText}> 
+          {this.state.myText}
+        </Text>
+        <TouchableOpacity
+        onPress={this.sendMessage}
+        >
+          <Text style={{fontSize: 30}}>MQTT</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +79,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 20,
+  }
 });
