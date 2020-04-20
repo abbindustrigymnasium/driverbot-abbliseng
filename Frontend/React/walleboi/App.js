abc59da96418a6baddcbcdfdf689ae5131181e11
Lisa Engström <Lisa.Engstrom@abbindustrigymnasium.se>
2020-04-20 09:04:43 +0200

    Update App.js

--- Frontend/React/walleboi/App.js
+++ Frontend/React/walleboi/App.js
@@ -1,19 +1,19 @@
 import React, { isValidElement, Component } from 'react';
-import { StyleSheet,
-   Text,
-   View, 
-   TouchableOpacity, 
-   AsyncStorage 
-  } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  AsyncStorage
+} from 'react-native';
 import init from './node_modules/react_native_mqtt'
-
 init({
   size: 10000,
   storageBackend: AsyncStorage,
   defaultExpires: 1000 * 3600 * 24,
   enableCache: true,
   reconnect: true,
-  sync : {
+  sync: {
 
   }
 })
@@ -21,13 +21,13 @@ function onConnect() {
   console.log("ye")
   client.subscribe("abbexpectmore@gmail.com/light")
 }
-function onConnectionLost(responseObject){
+function onConnectionLost(responseObject) {
   if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
+    console.log("onConnectionLost:" + responseObject.errorMessage);
   }
 }
 function onMessageArrived(message) {
-  console.log("onMessageArrived:"+message.payloadString);
+  console.log("onMessageArrived:" + message.payloadString);
 }
 const client = new Paho.MQTT.Client("maqiatto.com", 8883, 'clientID');
 // Set callback handlers
@@ -41,15 +41,15 @@ client.connect({
 })
 
 
-export default class reactApp extends Component{
-  constructor(){
+export default class reactApp extends Component {
+  constructor() {
     super()
     this.state = {
       myText: 'My Original Text'
     }
   }
   updateText = () => {
-    this.setState({myText: 'My changed text!'})
+    this.setState({ myText: 'My changed text!' })
   }
   sendMessage = () => {
     var mess = `(0:0:90)`
@@ -59,16 +59,16 @@ export default class reactApp extends Component{
     console.log(this.map(10, 0, 20, 0, 100))
   }
   map = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2;
-  render(){
-    return(
+  render() {
+    return (
       <View style={styles.container}>
-        <Text onPress = {this.updateText}> 
+        <Text onPress={this.updateText}>
           {this.state.myText}
         </Text>
         <TouchableOpacity
-        onPress={this.sendMessage}
+          onPress={this.sendMessage}
         >
-          <Text style={{fontSize: 30}}>MQTT</Text>
+          <Text style={{ fontSize: 30 }}>MQTT</Text>
         </TouchableOpacity>
       </View>
     )
