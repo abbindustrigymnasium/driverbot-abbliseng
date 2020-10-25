a7be0adf18c15c895875bb69215e16782ad179fa
Lisa Engström <Lisa.Engstrom@abbindustrigymnasium.se>
2020-10-25 15:14:30 +0100

    Hemsida klar

--- Frontend (Hemsida)/wall-e/src/components/HelloWorld.vue
+++ Frontend (Hemsida)/wall-e/src/components/HelloWorld.vue
@@ -1,48 +1,160 @@
 <template>
-  <div class="row justify-center" style="margin-top: 10px;">
-    <v-btn @click="send()" style="margin-right: 10px">Send</v-btn>
-    <h1 class="col-12">{{display}}</h1>
+  <div class="row justify-center" style="margin-top: 10px">
+    <div class="row col-12">
+      <v-btn style="height: 60px; margin-left: 20px; margin-right: 10px" color="primary" @click="send()">Update</v-btn>
+      <v-text-field label="Desired Speed" style="margin-right: 10px" v-model="dSpeed"></v-text-field>
+      <v-text-field label="Deriving Constant" style="margin-right: 10px" v-model="kd"></v-text-field>
+      <v-text-field label="Intregral Constant" style="margin-right: 20px" v-model="ki"></v-text-field>
+    </div>
+    <apexchart
+      class="col-12 trial"
+      type="line"
+      height="500"
+      :options="chartOptions"
+      :series="serie"
+    ></apexchart>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
+
 var mqtt = require("mqtt"),
   /* eslint-disable no-unused-vars */
-  url = require("url")
-var client = mqtt.connect('mqtt://maqiatto.com',{port: 8883, clientId: 'VueSite10923', username: "lisa.engstrom@abbindustrigymnasium.se", password: 'driverbot'})
-var data = "No recieved messages"
+  url = require("url");
 
-client.on('connect', function () {
-  client.subscribe('lisa.engstrom@abbindustrigymnasium.se/info', function (error) {
-    if (!error){
-      client.publish('lisa.engstrom@abbindustrigymnasium.se/info', 'Hello MQTT')
+var client = mqtt.connect("mqtt://maqiatto.com", {
+  port: 8883,
+  clientId: "VueSite10923",
+  username: "lisa.engstrom@abbindustrigymnasium.se",
+  password: "driverbot",
+});
+var data = "";
+
+client.on("connect", function () {
+  client.unsubscribe("lisa.engstrom@abbindustrigymnasium.se/ctrl")
+  client.subscribe("lisa.engstrom@abbindustrigymnasium.se/ctrl", function (
+    error
+  ) {
+    if (!error) {
+      client.publish(
+        "lisa.engstrom@abbindustrigymnasium.se/info",
+        "Website Connected!"
+      );
     }
-  })
-})
-client.on('message', function(topic, message) {
-  data = message.toString()
-})
+  });
+});
+client.on("message", function (topic, message) {
+  data = message.toString();
+});
 
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   data: () => ({
-    display: data,
+    display: "",
+    dSpeed: "",
+    kd: "",
+    ki: "",
+    series: [
+      {
+        name: "Desired speed",
+        data: [],
+      },
+      {
+        name: "Actual speed",
+        data: [],
+      },
+    ],
+    chartOptions: {
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
+          enabled: false,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      title: {
+        text: "Driverbot Speeds",
+        align: "center",
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5,
+        },
+      },
+      xaxis: {
+        labels: {
+          show: false
+        }
+      }
+    },
   }),
   methods: {
     send() {
-      let msg = String("Hello")
-      client.publish('lisa.engstrom@abbindustrigymnasium.se/info', msg);
+      if (this.dSpeed == ""){
+        this.dSpeed = '0'
+      }
+      if (this.kd == ""){
+        this.kd = '0'
+      }
+      if (this.ki == ""){
+        this.ki = '0'
+      }
+      firebase
+        .database()
+        .ref('kd')
+        .set(Number(this.kd))
+      firebase
+        .database()
+        .ref('ki')
+        .set(Number(this.ki))
+      let msg = String('('+this.dSpeed+':'+this.kd+':'+this.ki+')');
+      client.publish("lisa.engstrom@abbindustrigymnasium.se/info", msg);
+    },
+    data(newDataPoints) {
+      let x = []
+      this.series.forEach(line => {
+        x.push({name: line["name"], data: []})
+        line["data"].forEach(element => {
+          x[x.length-1]["data"].push(element)
+        });
+      });
+      x[0]["data"].push(newDataPoints[0])
+      x[1]["data"].push(newDataPoints[1])
+      this.series = x
     },
   },
   watch: {
-    display: function(value, oldValue) {
-      // console.log(oldValue)
-    }
+    display: function (value, oldValue) {
+      if (value != ""){
+        let formatted = value.replace('(','')
+        formatted = formatted.replace(')','')
+        const vars = formatted.split(',')
+        console.log(Number(vars[0]))
+        console.log(Number(vars[1]))
+        this.data([Number(vars[0]),Number(vars[1])])
+      }
+    },
   },
-  created: function(){
+  computed: {
+    serie() {
+      return this.series;
+    },
+  },
+  created: function () {
     setInterval(() => {
-      this.display = data
+      this.display = data;
     }, 1);
-  }
-}
+  },
+  beforeDestroyed: function () {
+    client.end();
+  },
+};
 </script>
--- Frontend (Hemsida)/wall-e/src/main.js
+++ Frontend (Hemsida)/wall-e/src/main.js
@@ -3,16 +3,24 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
-// import VueMqtt from 'vue-mqtt'
+import VueApexCharts from 'vue-apexcharts';
+import firebase from 'firebase';
 
-// Vue.use(VueMqtt, "maqiatto.com",
-//   {clientId: 'WebClient-' + parseInt(Math.random() * 100000),
-//   username: "abbexpectmore@gmail.com",
-//   password: "hej",
-//   port: 1883,
-// })
+Vue.use(VueApexCharts)
+Vue.component('apexchart', VueApexCharts)
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: "AIzaSyDxVCoKjLTKoyn5LCpQU90ARJHnhfWA-vs",
+  authDomain: "driverbotliseng.firebaseapp.com",
+  databaseURL: "https://driverbotliseng.firebaseio.com",
+  projectId: "driverbotliseng",
+  storageBucket: "driverbotliseng.appspot.com",
+  messagingSenderId: "78698869973",
+  appId: "1:78698869973:web:a8a74a495b9672a6fd9e5c"
+}
+firebase.initializeApp(config)
 
 new Vue({
   router,
